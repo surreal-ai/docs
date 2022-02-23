109077d56870a10b97a9fb4aaa18b603d9187b56
shonenada <shonenada@gmail.com>
2022-02-24 01:09:06 +0800

    chore: update config.js

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  lang: 'en-US',
+  title: 'Surreal Engine Developer Hub',
+  description: 'Surreal Engine Developer Hub',
+  seviceWorker: true,
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Surreal Engine Developer",
+    },
+  },
+  plugins: {
+    "tabs": true,
+    "@vuepress/back-to-top": true,
+    "@vuepress/last-updated": {
+      transformer: (timestamp, lang) => {
+        const dayjs = require("dayjs")
+        dayjs.locale(lang)
+        return dayjs(timestamp).format("YYYY-MM-DD HH:mm:ss")
+      },
+    },
+  },
+  markdown: {
+    lineNumbers: false,
+  },
+
+  // theme and its config
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    repo: "https://github.com/surreal-ai/docs",
+    editLinks: true,
+    editLinkText: "Edit this page on GitHub",
+    docsDir: "docs",
+  },
+}
