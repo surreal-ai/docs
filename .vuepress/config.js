f40b0385c629ee01f5870b07efe81c01c0c91960
shonenada <shonenada@gmail.com>
2022-02-23 13:59:34 +0800

    feat: introduce vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // site config
+  lang: 'en-US',
+  title: 'Surreal Engine',
+  description: 'Surreal Engine Developer Hub',
+
+  // theme and its config
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+  },
+}
---
+++ docs/README.md
@@ -0,0 +1,9 @@
+---
+lang: en-US
+title: Surreal Engine
+description: Surreal Engine
+---
+# Surreal Engine
+
+## What
+
