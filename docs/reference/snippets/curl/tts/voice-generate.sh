957f10597d6d7871e828f8cfff604c57378080a3
shonenada <shonenada@users.noreply.github.com>
2022-04-25 14:51:54 +0800

    fix voice name (#15)

--- docs/reference/snippets/curl/tts/voice-generate.sh
+++ docs/reference/snippets/curl/tts/voice-generate.sh
@@ -1,5 +1,5 @@
 curl -X POST \
     -H 'Authorization: Bearer bee03bed6352400383d9a51ba325af2f' \
     -H 'Content-Type: application/json' \
-    --data '{ "text": "Hello, Surreal Engine", "voice_name": "en-US-Wavenet-A" }'
+    --data '{ "text": "Hello, Surreal Engine", "voice_name": "Fabineu Topshot" }'
     https://openapi.surreal-ai.com/v1/tts/voice.generate
